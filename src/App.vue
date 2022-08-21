39e5c2b15406faf8f0b3776628a92764ee4535a1
ZonghengKaren <31526975+ZonghengKaren@users.noreply.github.com>
2022-08-21 18:10:05 +0800

    'fix'

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,14 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+      <h1>BASE</h1>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
   }
 }
 </script>
